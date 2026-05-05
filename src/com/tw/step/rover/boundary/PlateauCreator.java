e710f20fef1e7dbad1b0b9409bce8596c94e942c
scriptingmaverick <khasshaik12@gmail.com>
2026-05-05 23:08:25 +0530

    completes phase 2

--- src/com/tw/step/rover/boundary/PlateauCreator.java
+++ src/com/tw/step/rover/boundary/PlateauCreator.java
@@ -6,8 +6,8 @@ import com.tw.step.rover.roversystem.RoverSystemScanner;
 public class PlateauCreator {
 
   public static Plateau extractBoundaries(RoverSystemScanner scanner) {
-    Coordinate topRightCoord = scanner.scanBoundary();
     Coordinate bottomLeftCoord = new Coordinate(0, 0);
+    Coordinate topRightCoord = scanner.scanBoundary();
     return new Plateau(bottomLeftCoord, topRightCoord);
   }
 }
--- test/com/tw/step/rover/AppTest.java
+++ test/com/tw/step/rover/AppTest.java
@@ -8,18 +8,18 @@ import java.io.PrintStream;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AppTest {
-    @Test
-    void shouldPrintFinalRoverPosition() {
-        PrintStream originalOut = System.out;
-        ByteArrayOutputStream output = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(output));
+  @Test
+  void shouldPrintFinalRoverPosition() {
+    PrintStream originalOut = System.out;
+    ByteArrayOutputStream output = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(output));
 
-        try {
-            App.main();
-        } finally {
-            System.setOut(originalOut);
-        }
-
-        assertEquals("-4 6 N" + System.lineSeparator(), output.toString());
+    try {
+      App.main();
+    } finally {
+      System.setOut(originalOut);
     }
+
+    assertEquals("-1 2 N" + System.lineSeparator(), output.toString());
+  }
 }
